0d9779401c2af509742cb6612c05ecff64cf2dab
wusj <wesley.wusj@gmail.com>
2010-12-25 11:34:03 +0800

    delete redundant picture

--- src/org/wolink/m/android/driverexam/QuestionView.java
+++ src/org/wolink/m/android/driverexam/QuestionView.java
@@ -10,6 +10,8 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.res.AssetManager;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
@@ -65,7 +67,6 @@ public class QuestionView extends Activity implements OnClickListener, OnChecked
 	QuestionItem[] question_items; 
 	int[] question_list;
 	int[] answer_list;
-	int[] picsrc_list;
 	int[] wrong_list;
 	int current_question;
 	int current_wrong;
@@ -160,7 +161,6 @@ public class QuestionView extends Activity implements OnClickListener, OnChecked
 	    	finish();
 	    	return;
         }
-        initImageSrcList();
 		
         this.setTitle(getIntent().getStringExtra(KEY_TITLE));
         if (savedInstanceState == null) {
@@ -544,7 +544,16 @@ public class QuestionView extends Activity implements OnClickListener, OnChecked
 		
 		/* Update question image */
 		if (question_item.src != 0) { /* have a image */
-			imgv_picture.setImageResource(picsrc_list[question_item.src]);
+			BitmapDrawable drawable = null;
+			try {
+				AssetManager assets = getAssets(); 
+				drawable = new BitmapDrawable(getResources(), 
+					assets.open(question_item.src + ".gif"));
+			} 
+			catch (Throwable t) {
+				// Nothing
+			}
+			imgv_picture.setImageDrawable(drawable);
 		} else {
 			imgv_picture.setImageDrawable(null);
 		}
@@ -859,170 +868,6 @@ public class QuestionView extends Activity implements OnClickListener, OnChecked
         catch (Throwable t) {
         	return false;
         }		
-	}
-	
-	private void initImageSrcList() {
-		int[] src = {
-			0,
-			R.drawable.pic1,
-			R.drawable.pic2,
-			R.drawable.pic3,
-			R.drawable.pic4,
-			R.drawable.pic5,
-			R.drawable.pic6,
-			R.drawable.pic7,
-			R.drawable.pic8,
-			R.drawable.pic9,
-			R.drawable.pic10,
-			R.drawable.pic11,
-			R.drawable.pic12,
-			R.drawable.pic13,
-			R.drawable.pic14,
-			R.drawable.pic15,
-			R.drawable.pic16,
-			R.drawable.pic17,
-			R.drawable.pic18,
-			R.drawable.pic19,
-			R.drawable.pic20,
-			R.drawable.pic21,
-			R.drawable.pic22,
-			R.drawable.pic23,
-			R.drawable.pic24,
-			R.drawable.pic25,
-			R.drawable.pic26,
-			R.drawable.pic27,
-			R.drawable.pic28,
-			R.drawable.pic29,
-			R.drawable.pic30,
-			R.drawable.pic31,
-			R.drawable.pic32,
-			R.drawable.pic33,
-			R.drawable.pic34,
-			R.drawable.pic35,
-			R.drawable.pic36,
-			R.drawable.pic37,
-			R.drawable.pic38,
-			R.drawable.pic39,
-			R.drawable.pic40,
-			R.drawable.pic41,
-			R.drawable.pic42,
-			R.drawable.pic43,
-			R.drawable.pic44,
-			R.drawable.pic45,
-			R.drawable.pic46,
-			R.drawable.pic47,
-			R.drawable.pic48,
-			R.drawable.pic49,
-			R.drawable.pic50,
-			R.drawable.pic51,
-			R.drawable.pic52,
-			R.drawable.pic53,
-			R.drawable.pic54,
-			R.drawable.pic55,
-			R.drawable.pic56,
-			R.drawable.pic57,
-			R.drawable.pic58,
-			R.drawable.pic59,
-			R.drawable.pic60,
-			R.drawable.pic61,
-			R.drawable.pic62,
-			R.drawable.pic63,
-			R.drawable.pic64,
-			R.drawable.pic65,
-			R.drawable.pic66,
-			R.drawable.pic67,
-			R.drawable.pic68,
-			R.drawable.pic69,
-			R.drawable.pic70,
-			R.drawable.pic71,
-			R.drawable.pic72,
-			R.drawable.pic73,
-			R.drawable.pic74,
-			R.drawable.pic75,
-			R.drawable.pic76,
-			R.drawable.pic77,
-			R.drawable.pic78,
-			R.drawable.pic79,
-			R.drawable.pic80,
-			R.drawable.pic81,
-			R.drawable.pic82,
-			R.drawable.pic83,
-			R.drawable.pic84,
-			R.drawable.pic85,
-			R.drawable.pic86,
-			R.drawable.pic87,
-			R.drawable.pic88,
-			R.drawable.pic89,
-			R.drawable.pic90,
-			R.drawable.pic91,
-			R.drawable.pic92,
-			R.drawable.pic93,
-			R.drawable.pic94,
-			R.drawable.pic95,
-			R.drawable.pic96,
-			R.drawable.pic97,
-			R.drawable.pic98,
-			R.drawable.pic99,
-			R.drawable.pic100,
-			R.drawable.pic101,
-			R.drawable.pic102,
-			R.drawable.pic103,
-			R.drawable.pic104,
-			R.drawable.pic105,
-			R.drawable.pic106,
-			R.drawable.pic107,
-			R.drawable.pic108,
-			R.drawable.pic109,
-			R.drawable.pic110,
-			R.drawable.pic111,
-			R.drawable.pic112,
-			R.drawable.pic113,
-			R.drawable.pic114,
-			R.drawable.pic115,
-			R.drawable.pic116,
-			R.drawable.pic117,
-			R.drawable.pic118,
-			R.drawable.pic119,
-			R.drawable.pic120,
-			R.drawable.pic121,
-			R.drawable.pic122,
-			R.drawable.pic123,
-			R.drawable.pic124,
-			R.drawable.pic125,
-			R.drawable.pic126,
-			R.drawable.pic127,
-			R.drawable.pic128,
-			R.drawable.pic129,
-			R.drawable.pic130,
-			R.drawable.pic131,
-			R.drawable.pic132,
-			R.drawable.pic133,
-			R.drawable.pic134,
-			R.drawable.pic135,
-			R.drawable.pic136,
-			R.drawable.pic137,
-			R.drawable.pic138,
-			R.drawable.pic139,
-			R.drawable.pic140,
-			R.drawable.pic141,
-			R.drawable.pic142,
-			R.drawable.pic143,
-			R.drawable.pic144,
-			R.drawable.pic145,
-			R.drawable.pic146,
-			R.drawable.pic147,
-			R.drawable.pic148,
-			R.drawable.pic149,
-			R.drawable.pic150,
-			R.drawable.pic151,
-			R.drawable.pic152,
-			R.drawable.pic153,
-			R.drawable.pic154,
-			R.drawable.pic155,
-			R.drawable.pic156,
-			R.drawable.pic157,
-		};
-		picsrc_list = src; 
 	}
 	
 	private int byteToInt(byte bh, byte bl) {
